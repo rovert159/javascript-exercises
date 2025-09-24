28f572cfba9d6196763c5adff8897f9f03b9ed2e
Roland Hunt <rovert159@hotmail.com>
2025-09-24 09:57:40 -0230

    Complete the exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,24 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+    if (typeof string !== "string") return "ERROR, Your word is not a String";
+    if (!(Number.isInteger(num) && num >= 0)) return "ERROR"
+    // check to make sure valid variables were passed in
+
+    let newString = '' // initiate a string
+
+    for (let i = 0; i < num; i++) {
+        newString += string;
+    }
+    // Adds a copy of string onto newString each iteration
+    // Iterates a number of times equal to num
+
+    return newString;
+
+let word = 'bubble';
+let digit = 6;
+
+console.log(repeatString(word, digit));
+
 
 };
 
