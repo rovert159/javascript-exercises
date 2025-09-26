881f3a595058f28d5372243df1be0e53528424bd
Roland Hunt <rovert159@hotmail.com>
2025-09-26 10:18:51 -0230

    complete removeFromArray Exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(listOfThings, ...blackList) {
+    //let revisedListOfThings = [];
+    for (let i = 0; i < blackList.length; i++) {
+        listOfThings = listOfThings.filter(item => item !== blackList[i]);
+    }
+    return listOfThings;
 };
 
 // Do not edit below this line
