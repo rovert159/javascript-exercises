7e873480b3afd23c5f18e8bccdf8df2b81dc81a9
Roland Hunt <rovert159@hotmail.com>
2025-09-26 09:51:49 -0230

    Complete the reverseString Exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    array = string.split('');
+    newString = '';
+    for (let i = array.length; i > 0; i--) {
+        newString += array[i-1];
+    }
+    return newString;
 };
 
 // Do not edit below this line
